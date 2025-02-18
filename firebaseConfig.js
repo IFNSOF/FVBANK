eb40f9261beb165be6edb1fd0a41e4d484e3493f
Tweegy <159832171+IFNSOF@users.noreply.github.com>
2025-02-18 17:46:59 +0200

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// Export Firebase config if needed
+const firebaseConfig = {
+    apiKey: "AIzaSyBu3ie7Yk6JGtM1Kzag8i3lfuitEipJvsk",
+    authDomain: "fvbank-e7920.firebaseapp.com",
+    projectId: "fvbank-e7920",
+    storageBucket: "fvbank-e7920.firebasestorage.app",
+    messagingSenderId: "955965017278",
+    appId: "1:955965017278:web:2494c170bff3cbed0622ef"
+};
+
+export default firebaseConfig;
